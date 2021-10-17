e555160a9d60797d5db05d73a3742b7bb8ae7f54
koshiko19 <a.koshiko@leihauoli.com>
2021-10-17 21:41:44 +0900

    条件分岐とループまで

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
 
-Vue.config.productionTip = false
+const HelloVueApp = {
+	data() {
+		return {
+			message: 'Hello Vue!!'
+		}
+	}
+}
 
-new Vue({
-	render: h => h(App),
-}).$mount('#app')
+Vue.createApp(HelloVueApp).mount('#hello-vue')
